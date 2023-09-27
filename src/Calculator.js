b2f84d2d9b2afae3f932d89f418fa89e427e37df
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-09-27 22:53:34 +0530

    CSS

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,26 +6,34 @@ function Calculator() {
 
 
   return (
-    <div className='flex justify-center'>
-    <div className='w-1/2 flex flex-col '>
-      <p className='screen text-center'>{output}</p>
-      <div className='button grid grid-cols-3'>
-            <button onClick={()=>{setOutput('')}}> AC </button>
-            <button onClick={()=>{setOutput(0)}}> CE </button>
-            <button onClick={()=>{setOutput(output.slice(0 , -1))}}> DEL </button>
-            <button onClick={()=>{setOutput(output+9)}}>9</button>
-            <button onClick={()=>{setOutput(output+8)}}>8</button>
-            <button onClick={()=>{setOutput(output+7)}}>7</button>
-            <button onClick={()=>{setOutput(output+6)}}>6</button>
-            <button onClick={()=>{setOutput(output+5)}}>5</button>
-            <button onClick={()=>{setOutput(output+4)}}>4</button>
-            <button onClick={()=>{setOutput(output+3)}}>3</button>
-            <button onClick={()=>{setOutput(output+2)}}>2</button>
-            <button onClick={()=>{setOutput(output+1)}}>1</button>
+    <div className='flex justify-center mt-10'>
+    <div className='w-1/2 flex flex-col gap-2 text-white text-xl'>
+      <p className='screen text-center h-24 bg-slate-300 rounded-md text-black text-5xl flex items-center justify-center'>{output}</p>
+      <div className='button 
+                      grid 
+                      grid-cols-3 
+                      gap-3
+                      '>
+            
+   
+            
+            
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput('')}}> AC </button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(0)}}> CE </button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output.slice(0 , -1))}}> DEL </button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+9)}}>9</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+8)}}>8</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+7)}}>7</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+6)}}>6</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+5)}}>5</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+4)}}>4</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+3)}}>3</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+2)}}>2</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+1)}}>1</button>
 
-            <button onClick={()=>{setOutput(output+'-')}}> -</button>
-            <button onClick={()=>{setOutput(output+'+')}}> + </button>  
-            <button onClick={()=>{setOutput(eval(output))}}> =</button> 
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+'-')}}> -</button>
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(output+'+')}}> + </button>  
+            <button className='h-16 bg-slate-600 rounded-md' onClick={()=>{setOutput(eval(output))}}> =</button> 
       </div>  
 
 
